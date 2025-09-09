7a4889ed2011b38ba1fcd1a1c5d3a707b2637996
SukheeYnwa <sukheearch@gmail.com>
2025-09-09 16:40:13 +0800

    commit

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
 const game = (() => {
-    const gameBoard = [0,1,2,3,4,5,6,7,8];
     const winningCombination = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,5,8], [3,4,5], [6,7,8], [2,4,6]];
+    const winnerModal = document.querySelector('#winner-modal');
+    const winnerModalTitle = document.querySelector('.title');
+    const resetButton = document.querySelector('#reset-button');
+    const statusTitle = document.querySelector('#status-title');
+    const statusCell = document.querySelector('.status-cell');
+    const grid = document.querySelectorAll('.cell');
 
-    function checkWinner(points) {
-        return winningCombination.some(
-            combo => combo.every(i => points.includes(i))
-        );
+    const player1 = createPlayer('sukhee', 'X');
+    const player2 = createPlayer('steve', 'O');
+
+    let gameBoard = [0,1,2,3,4,5,6,7,8];
+    let currentPlayer = player2;
+    
+    function createPlayer(name, marker) {
+        const points = [];
+        return {name, marker, points};
     };
 
-    function createPlayer(name, marker) {
-        let points = [];
-        addPoint = (i) => {
-            const index = gameBoard.indexOf(i);
-            gameBoard.splice(index, 1);
-            points.push(i);
-            if(checkWinner(points)) {
-                console.log(`${name} won!`);
-            };
+    function checkWinner(player) {
+        if (winningCombination.some(combo => combo.every(i => player.points.includes(i)))) {
+            winnerModalTitle.textContent = `${player.name.toUpperCase()} WON!`;
+            winnerModal.showModal();
+        } else {
+            checkBoard(gameBoard);
         };
-        getPoint = () => {
-            console.log(points);
+    };
+
+    function checkBoard(board) {
+        if (board.length === 0) {
+            winnerModalTitle.textContent = `GAME TIED!`;
+            winnerModal.showModal();
         };
-        return {name, marker, addPoint, getPoint};
+    };
+
+    function resetGame() {
+        gameBoard = [0,1,2,3,4,5,6,7,8];
+        player1.points = [];
+        player2.points = [];
+        grid.forEach(cell => {
+            cell.textContent = '';
+            cell.className = 'cell';
+        });
+        winnerModal.close();
+    };
+
+    function addPoint(player, i) {
+        const index = gameBoard.indexOf(i);
+        gameBoard.splice(index, 1);
+        player.points.push(i);
     };
 
     function togglePlayer() {
@@ -30,50 +57,26 @@ const game = (() => {
         } else {
             currentPlayer = player1;
         };
+        statusCell.textContent = currentPlayer.marker;
+        statusCell.className = `status-cell ${currentPlayer.marker}`;
+        statusTitle.textContent = `${currentPlayer.name.toUpperCase()}'S TURN`
     };
-    
-    const player1 = createPlayer('sukhee', 'x');
-    const player2 = createPlayer('steve', 'o');
-    let currentPlayer = player1;
-
-    const grid = document.querySelectorAll('.cell');
 
     grid.forEach((cell, i) => {
         cell.addEventListener('click', (e)=> {
             if(gameBoard.includes(i)) {
                 e.target.textContent = currentPlayer.marker;
-                currentPlayer.addPoint(i);
+                e.target.className = `cell ${currentPlayer.marker}`;
+                addPoint(currentPlayer, i);
+                checkWinner(currentPlayer);
                 togglePlayer();
-                console.log(gameBoard);
-            } else {
-                console.log("CHOOSE AGAIN!")
-            }
+            };
         });
     });
 
-    return {gameBoard, createPlayer, currentPlayer};
+    resetButton.addEventListener('click', resetGame);
+
+    togglePlayer();
+
+    return {};
 })();
-
-
-
-
-
-/* 
-012
-345
-678
-
-div deer daraad 
-
-- player uud uuriin gesen mark tai bn
-- player uud daraallasan songolt hiij bolohgui
-
-
-- player uud eeljilj songolt hiine
-- playeriin markiig awch gameBoard luu shahna
-- gameBoard ylagchiin pattern bgaag shalgana
-- pattern taarsan tohioldold togloom duusna
-- pattern taaraagui bol daraagiin toglogch songolt hiine
-
-
-*/
